30f78f1e56cae2c87cc84c3d756c2149f745f6bb
HANP <63062155+HANP@users.noreply.github.com>
2025-09-05 13:11:54 +0800

    1

--- repo-template-1.20.1/src/main/java/com/lvvfll/repo/Repo.java
+++ repo-template-1.20.1/src/main/java/com/lvvfll/repo/Repo.java
@@ -1,24 +1,24 @@
-package com.lvvfll.repo;
+package com.example.repo; // 你的包名
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.bernie.geckolib.GeckoLib; // 导入 GeckoLib
 
 public class Repo implements ModInitializer {
-	public static final String MOD_ID = "repo";
+    // 这在 Fabric 的所有实例中运行。
+    // 请注意，客户端和服务器代码都在这里运行！
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "repo"; // 你的 Mod ID
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    @Override
+    public void onInitialize() {
+        // 这是你的模组在加载时运行的第一个东西。
+        // 你可以记录一个消息，等等。
+        LOGGER.info("Hello Fabric world!");
 
-		LOGGER.info("Hello Fabric world!");
-	}
+        // 初始化 GeckoLib
+        GeckoLib.initialize(); // 添加这行
+    }
 }
